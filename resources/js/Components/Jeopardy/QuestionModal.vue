9cd6294f2ae97c14eb63ff109d19a35cabf2784d
Mark Lewellyn <lewellym4243@gmail.com>
2025-07-20 10:15:38 -0500

    Add answer button

--- resources/js/Components/Jeopardy/QuestionModal.vue
+++ resources/js/Components/Jeopardy/QuestionModal.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
 const props = defineProps({
     question: Object
 })
 
 const emit = defineEmits(['close'])
+const showAnswer = ref(false);
 const close = () => emit('close')
+const toggleAnswer = () => {
+    showAnswer.value = !showAnswer.value;
+}
 </script>
 
 <template>
@@ -18,13 +24,22 @@ const close = () => emit('close')
         >
             <div class="bg-white p-8 rounded-lg shadow-2xl text-center max-w-2xl w-full">
                 <h2 class="text-2xl font-bold mb-4 text-gray-800">{{ question.category }}</h2>
-                <p class="text-xl text-gray-700">{{ question.question }}</p>
-                <button
-                    class="mt-6 px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-800"
-                    @click="close"
-                >
-                    Close
-                </button>
+                <p v-if="!showAnswer" class="text-xl text-gray-700">{{ question.question }}</p>
+                <p v-if="showAnswer" class="text-xl text-gray-700">{{ question.answer }} - {{ question.reference }}</p>
+                <div class="flex flex-row justify-around gap-x-4">
+                    <button
+                        class="mt-6 px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-800"
+                        @click="toggleAnswer"
+                    >
+                        Reveal Answer
+                    </button>
+                    <button
+                        class="mt-6 px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-800"
+                        @click="close"
+                    >
+                        Close
+                    </button>
+                </div>
             </div>
         </transition>
     </div>
