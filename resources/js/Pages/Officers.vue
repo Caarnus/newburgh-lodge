3cc522232aa1ceaf81e826ac5e259a0fe2a6c068
Mark Lewellyn <lewellym4243@gmail.com>
2026-02-08 15:04:51 -0600

    Update officers list

--- resources/js/Pages/Officers.vue
+++ resources/js/Pages/Officers.vue
@@ -30,15 +30,15 @@ import AppLayout from "@/Layouts/AppLayout.vue";
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Senior Warden:</td>
-                                        <td class="px-4 py-2">Mark Lewellyn, PM</td>
+                                        <td class="px-4 py-2">Tom Metzger</td>
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Junior Warden:</td>
-                                        <td class="px-4 py-2">Tom Metzger, PM</td>
+                                        <td class="px-4 py-2">Bryan Bodkin</td>
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Treasurer:</td>
-                                        <td class="px-4 py-2">Chadd Huffman</td>
+                                        <td class="px-4 py-2">Tom Donnelly</td>
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Secretary:</td>
@@ -46,7 +46,7 @@ import AppLayout from "@/Layouts/AppLayout.vue";
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Senior Deacon:</td>
-                                        <td class="px-4 py-2">Jonathan Harris</td>
+                                        <td class="px-4 py-2">Mark Lewellyn</td>
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Junior Deacon:</td>
@@ -54,30 +54,30 @@ import AppLayout from "@/Layouts/AppLayout.vue";
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Senior Steward:</td>
-                                        <td class="px-4 py-2">Bryan Bodkin</td>
+                                        <td class="px-4 py-2">Brandon Bergner</td>
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Junior Steward:</td>
-                                        <td class="px-4 py-2">Open</td>
+                                        <td class="px-4 py-2">Jeffery McCarroll</td>
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Tyler:</td>
-                                        <td class="px-4 py-2">Rob Harris</td>
+                                        <td class="px-4 py-2">Open</td>
                                     </tr>
                                     <tr>
                                         <td class="px-4 py-2 text-right">Chaplain:</td>
-                                        <td class="px-4 py-2">Robert Barnes</td>
+                                        <td class="px-4 py-2">Bob Barnes</td>
                                     </tr>
                                     <tr>
-                                        <td class="px-4 py-2 text-right">Trustee 1:</td>
-                                        <td class="px-4 py-2">Ken Mitz, PM</td>
+                                        <td class="px-4 py-2 text-right">Trustee:</td>
+                                        <td class="px-4 py-2">Tom Donnelly</td>
                                     </tr>
                                     <tr>
-                                        <td class="px-4 py-2 text-right">Trustee 2:</td>
+                                        <td class="px-4 py-2 text-right">Trustee:</td>
                                         <td class="px-4 py-2">Rod McDonald</td>
                                     </tr>
                                     <tr>
-                                        <td class="px-4 py-2 text-right">Trustee 3:</td>
+                                        <td class="px-4 py-2 text-right">Trustee:</td>
                                         <td class="px-4 py-2">Tim Putnam</td>
                                     </tr>
                                     </tbody>
