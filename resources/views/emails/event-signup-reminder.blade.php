7312daf5fc3be61ec8f60e1cffa278560e554b80
Mark Lewellyn <lewellym4243@gmail.com>
2026-03-03 13:26:48 -0600

    Fix markdown indentation on other email

--- resources/views/emails/event-signup-reminder.blade.php
+++ resources/views/emails/event-signup-reminder.blade.php
@@ -1,30 +1,32 @@
 @php
-    $local = $occurrenceStartUtc->timezone($timezone);
+$local = $occurrenceStartUtc->timezone($timezone);
+
+$startsIn = match ($reminderType) {
+    'week' => 'in 1 week',
+    'day' => 'in 1 day',
+    'hour' => 'in 1 hour',
+    default => 'soon',
+};
 @endphp
 
 @component('mail::message')
 # {{ $eventTitle }}
 
-This is a reminder that **{{ $eventTitle }}** starts {{ match($reminderType) {
-'week' => 'in 1 week',
-'day' => 'in 1 day',
-'hour' => 'in 1 hour',
-default => 'soon',
-} }}.
+This is a reminder that **{{ $eventTitle }}** starts {{ $startsIn }}.
 
 **When:** {{ $local->format('l, F j, Y \a\t g:i A') }} ({{ $timezone }})
 @if(!empty($location))
-    **Where:** {{ $location }}
+**Where:** {{ $location }}
 @endif
 
 @component('mail::button', ['url' => $manageUrl])
-    Manage reminders
+Manage reminders
 @endcomponent
 
 If you no longer want reminders for this event, you can remove your signup:
 
 @component('mail::button', ['url' => $unsubscribeUrl])
-    Remove my signup
+Remove my signup
 @endcomponent
 
 Thanks,<br>
