73644a0a41cf960d06e81ce10cc6d0f76be0a798
Mark Lewellyn <lewellym4243@gmail.com>
2026-03-03 13:01:06 -0600

    Fix markdown indentation

--- resources/views/emails/event-signup-reminder.blade.php
+++ resources/views/emails/event-signup-reminder.blade.php
@@ -3,30 +3,30 @@
 @endphp
 
 @component('mail::message')
-    # {{ $eventTitle }}
+# {{ $eventTitle }}
 
-    This is a reminder that **{{ $eventTitle }}** starts {{ match($reminderType) {
-    'week' => 'in 1 week',
-    'day' => 'in 1 day',
-    'hour' => 'in 1 hour',
-    default => 'soon',
+This is a reminder that **{{ $eventTitle }}** starts {{ match($reminderType) {
+'week' => 'in 1 week',
+'day' => 'in 1 day',
+'hour' => 'in 1 hour',
+default => 'soon',
 } }}.
 
-    **When:** {{ $local->format('l, F j, Y \a\t g:i A') }} ({{ $timezone }})
-    @if(!empty($location))
-        **Where:** {{ $location }}
-    @endif
+**When:** {{ $local->format('l, F j, Y \a\t g:i A') }} ({{ $timezone }})
+@if(!empty($location))
+    **Where:** {{ $location }}
+@endif
 
-    @component('mail::button', ['url' => $manageUrl])
-        Manage reminders
-    @endcomponent
+@component('mail::button', ['url' => $manageUrl])
+    Manage reminders
+@endcomponent
 
-    If you no longer want reminders for this event, you can remove your signup:
+If you no longer want reminders for this event, you can remove your signup:
 
-    @component('mail::button', ['url' => $unsubscribeUrl])
-        Remove my signup
-    @endcomponent
+@component('mail::button', ['url' => $unsubscribeUrl])
+    Remove my signup
+@endcomponent
 
-    Thanks,<br>
-    {{ config('app.name') }}
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
--- resources/views/emails/scholarship/verify.blade.php
+++ resources/views/emails/scholarship/verify.blade.php
@@ -1,18 +1,18 @@
 @component('mail::message')
-    # Verify your scholarship application
+# Verify your scholarship application
 
-    Hi {{ $application->first_name }},
+Hi {{ $application->first_name }},
 
-    Please verify your email address to submit your scholarship application.
+Please verify your email address to submit your scholarship application.
 
-    @component('mail::button', ['url' => $verifyUrl])
-        Verify Application
-    @endcomponent
+@component('mail::button', ['url' => $verifyUrl])
+    Verify Application
+@endcomponent
 
-    This link will expire in 48 hours.
+This link will expire in 48 hours.
 
-    If you did not request this, you can ignore this email.
+If you did not request this, you can ignore this email.
 
-    Thanks,<br>
-    {{ config('app.name') }}
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
